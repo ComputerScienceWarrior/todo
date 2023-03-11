7dbefc1ad23e232b88691d21e4bacb4bb0470271
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-03-10 17:41:47 -0700

    Displays <Item/> component

---
+++ src/components/Item.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Item = () => {
+    // const style={
+    //     fontWeight: "bold",
+    //     color: "black",
+    //     border: "solid red 1px",
+    //     borderRadius: "5px",
+    //     width: "75%",
+    //     margin: "auto",
+    //     padding: "15px",
+    //     display: "flex",
+    //     flexDirection: "row"
+    // }
+    return(
+        <div className="flex flex-col">
+            <div>Item Component</div>
+            <div>X</div>
+        </div>
+    )
+}
+
+export default Item;
