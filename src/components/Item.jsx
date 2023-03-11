49708318e89152ed6a8b42133a598c5127841e58
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-03-10 19:23:11 -0700

    Adds in use of props

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,23 +1,12 @@
 import React from "react";
 
-const Item = () => {
-    // const style={
-    //     fontWeight: "bold",
-    //     color: "black",
-    //     border: "solid red 1px",
-    //     borderRadius: "5px",
-    //     width: "75%",
-    //     margin: "auto",
-    //     padding: "15px",
-    //     display: "flex",
-    //     flexDirection: "row"
-    // }
+const Item = (props) => {
     return(
         <div className="border-2 border-black p-10 m-5 rounded bg-red-400 font-bold">
-            <h1 className="text-2xl italic text-white underline underline-offset-4">List Item Name</h1>
-            <div className="flex flex-row justify-between"> {/* props.itemName  */}
-                <div className="float-left">Item Component</div>
-                <div className="float-right w-10 cursor-pointer" onClick={() => console.log("Closed")}>X</div>
+            <h1 className="text-2xl italic text-white underline underline-offset-4">{props.name}</h1>
+            <div className="flex flex-row justify-between">
+                <div className="float-left">{props.description}</div>
+                <div className="float-right w-10 cursor-pointer" onClick={() => console.log(`Closed task '${props.name}'.`)}>X</div>
             </div>
         </div>
     )
