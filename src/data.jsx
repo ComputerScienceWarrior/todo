56f065f4efab57010e280519659350d5eb47594f
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2023-03-10 19:23:58 -0700

    creates temporary data to work with

---
+++ src/data.jsx
@@ -0,0 +1,7 @@
+
+export const data = [
+    {name: "Task 1", description: "Create a to-do list application."},
+    {name: "Task 2", description: "Finish making a portfolio website."},
+    {name: "Task 3", description: "Create CRM application for portfolio."},
+    {name: "Task 4", description: "Finsh 1 section of AWS Cloud Practioner"},
+]
